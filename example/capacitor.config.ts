3f5ea0b86e28c9e4c1536a08f2c53019dc870aa1
Shiva Prasad <sp@shiv19.com>
2024-06-12 11:21:47 +1200

    Update capacitor.config.ts

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -3,8 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'example',
-  webDir: 'www',
-  backgroundColor: '#ff000000'
+  webDir: 'www'
 };
 
 export default config;
