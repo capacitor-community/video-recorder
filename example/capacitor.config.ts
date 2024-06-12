d79f2b9e5f4d88896b2e5149ebacc10f4f923993
Shiva Prasad <sp@shiv19.com>
2024-06-12 17:38:03 +1200

    Update capacitor.config.ts
    adding this back as it is required on iOS

--- example/capacitor.config.ts
+++ example/capacitor.config.ts
@@ -3,7 +3,9 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
   appName: 'example',
-  webDir: 'www'
+  webDir: 'www',
+  // This is required for the @capacitor-community/video-recorder plugin to work on iOS
+  backgroundColor: '#ff000000',
 };
 
 export default config;
